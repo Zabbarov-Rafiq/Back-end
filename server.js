5a6a7024deac51ec2d55401bab022a0f57d3db18
Rafiq_Java <zabbarovrafiq@gmail.com>
2026-08-06 15:45:17 +0400

    Fix CORS

--- server.js
+++ server.js
@@ -1,31 +1,36 @@
 const dns = require("dns");
-
 dns.setDefaultResultOrder("ipv4first");
+
 require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 
 const app = express();
 
+// MongoDB
 connectDB();
 
-const PORT = process.env.PORT || 3000;
+// Middleware
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type"]
+}));
 
 app.use(express.json());
-app.use(cors({
-  origin: "https://premium-cars-frontend.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type"]
-}));
-app.use(cors());
 
+// Routes
 const carRoutes = require("./routes/carRoutes");
 app.use("/api/cars", carRoutes);
 
+// Test route
 app.get("/", (req, res) => {
     res.send("PremiumCars Backend 🚗");
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
